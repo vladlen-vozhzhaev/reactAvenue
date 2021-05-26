64810ce3a218afd5515ffe8e2d01ba38beab39c4
Vladlen <vladlen@vozhzhaev.ru>
2021-05-26 21:23:41 +0300

    Использование шаблона clean blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter, NavLink, Route} from "react-router-dom";
+import {Main} from "./components/Main";
+import {About} from "./components/About";
+import {Contact} from "./components/Contact";
+import {Menu} from "./components/Menu";
+import {PageHeader} from "./components/PageHeader";
+import {Footer} from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+            <Menu/>
+            <PageHeader/>
+            <Route exact path="/">
+                <Main/>
+            </Route>
+            <Route path="/about">
+                <About/>
+            </Route>
+            <Route path="/contact-us">
+                <Contact/>
+            </Route>
+            <Footer/>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,5 @@
+export function Contact(){
+    return (
+        <h1>Контакты</h1>
+    )
+}
